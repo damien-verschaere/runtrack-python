8f16932a89192e5963b54110901bfb3a90625974
damien-verschaere <damien.verschaere@laplateforme.io>
2023-03-29 09:53:35 +0200

    jour02 done job AI done

--- jour01/job13.py
+++ jour01/job13.py
@@ -1,9 +1,14 @@
-nombres = []
-for i in range(5):
-    nombre = int(input("Entrez un nombre entier : "))
-    nombres.append(nombre)
 
-nombres.sort()
-print("Voici les nombres triés par ordre croissant :")
-for nombre in nombres:
+
+nbrs = []
+for i in range(10):
+    nombre = int(input("Entrez un nombre entier : "))
+    nbrs.append(nombre)
+
+
+
+#fonction de triage 
+nbrs.sort()
+print("Voici les nombres triés par ordre croissant!")
+for nombre in nbrs:
     print(nombre)
